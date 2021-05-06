da702f49add47148ce22abe250db60d2efefba33
haki9975 <elias.robert.hakim@gmail.com>
2021-05-05 21:43:14 -0400

    commit tree 0121874c6e72af992459a80b4ae0ac46806ccadb

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+    //const people = [ 'Lisa', 'Kaitlin', 'Jan' ]
+    const cards = []
+
+function writeCards(array, string) {
+    for (let f = 0; f < array.length; f++ ) {
+        cards.push(`Thank you, ${array[f]} for the wonderful ${string} gift!`);
+        return cards;
+    };
+};
+
+
+function countDown(number) {
+    for (let count = number; count >= 0; count --) {
+        console.log(`${count}`);
+    }
+};
